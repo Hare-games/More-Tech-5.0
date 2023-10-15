993673b85c9d58e76137e390df19fc4de0723834
Александр <fursin2002@mail.ru>
2023-10-15 07:39:25 +0400

    #Made All screens

---
+++ Assets/Scripts/Screens/ToggleButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleButton : MonoBehaviour
+{
+    public Color OnState;
+    public Color OffState;
+    public bool IsActive = false;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => { ChangeColor(); });
+    }
+
+    void ChangeColor()
+    {
+        IsActive = !IsActive;
+        if (IsActive)
+        {
+            GetComponent<Image>().color = OnState;
+        }
+        else
+        {
+            GetComponent<Image>().color = OffState;
+        }
+    }
+}
